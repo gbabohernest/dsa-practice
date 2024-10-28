f6065cd8fc62b105b21b109a0ff0cc64dcf61c28
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2024-10-28 12:29:32 +0300

    Return the sum of a given linked list

---
+++ PHP/LinkedList/sum_of_a_linked_list.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Calculate and return sum of a given LinkedList.
+ * Iterative & Recursive Approach.
+ */
+
+
+class Node
+{
+    public int $value;
+    public $next;
+
+    public function __construct(int $value)
+    {
+        $this->value = $value;
+        $this->next = null;
+    }
+
+    public static function get_LL_sum(Node $head): int
+    {
+        $sum = 0;
+        $current_node = $head;
+
+        while ($current_node !== null) {
+            $sum += $current_node->value;
+            $current_node = $current_node->next;
+        }
+
+        return $sum;
+    }
+
+}
+
+
+$a = new Node(10);
+$b = new Node(30);
+$c = new Node(40);
+$d = new Node(50);
+
+$a->next = $b;
+$b->next = $c;
+$c->next = $d;
+
+var_dump(Node::get_LL_sum($a)); # Output: 130
