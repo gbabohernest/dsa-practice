0cc1dba019872d1a6a1997eb3e3866ed7f279bf6
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2024-11-05 16:45:07 +0300

    Recursively insert a new node in the LL

--- python/LinkedList/insert_at_a_given_index.py
+++ python/LinkedList/insert_at_a_given_index.py
@@ -5,6 +5,8 @@ Given the head of a LL, an index, and a value. Write a function that insert a gi
 node in the  LL at a given index and return the head of the List.
 """
 
+from typing import Any
+
 
 class Node:
     def __init__(self, data: int):
@@ -12,13 +14,44 @@ class Node:
         self.next = None
 
     @staticmethod
+    def insert_at_given_index_recursively(head: 'Node', value: int, index: int) -> 'Node' or None:
+        """
+        Recursively insert a new node at a given index in the single LL
+        :param head: current head of the LL
+        :param value: value to insert
+        :param index: index at which to insert the value
+        :return: updated head of the LL
+
+        """
+
+        if head is None:
+            print('Index out of bound, cannot insert')
+            exit()
+
+        if head is None and index == 0:
+            # insert at the beginning of the list
+            new_node = Node(value)
+            new_node.next = head
+            head = new_node
+            return head
+
+        if index == 0:
+            new_node = Node(value)
+            new_node.next = head
+            return new_node
+
+        head.next = Node.insert_at_given_index_recursively(head.next, value, index - 1)
+        return head
+
+    @staticmethod
     def insert_at_given_index_iteratively(head: 'Node', value: int, index: int) -> 'Node':
         """
         Inserts a node in a given LL at a particular index.
-        :param head: Head of the LL
-        :param value: Node's value
-        :param index: Index to insert the new node
-        :return: LL head node
+
+        :param head: current head of the LL
+        :param value: value to insert
+        :param index: index at which to insert the value
+        :return: updated head of the LL
         """
 
         if index == 0:
@@ -47,6 +80,7 @@ class Node:
         else:
             # index is out of bound
             print('Index is out of bound, cannot insert a new node')
+            exit()
 
         return head
 
@@ -75,3 +109,5 @@ d.next = f
 Node.print_linkedlist(Node.insert_at_given_index_iteratively(a, 5, 4))  # 1 -> 2 -> 3 -> 4 -> 5 -> 6
 print()
 Node.print_linkedlist(Node.insert_at_given_index_iteratively(a, 0, 0))  # 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6
+print()
+Node.print_linkedlist(Node.insert_at_given_index_recursively(a, 55, 3))  # 1 -> 2 -> 3 -> 55 -> 4 -> 5 -> 6
