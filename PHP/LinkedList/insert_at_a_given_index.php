6f434164447d15e4e92b3e81cc749554493c4654
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2024-11-05 19:21:10 +0300

    insert a node in a single linked list at a given index and return the head node

---
+++ PHP/LinkedList/insert_at_a_given_index.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ ** Given the head of a LL, an index, and a value. Write a function that insert a given
+ ** node in the  LL at a given index and return the head of the List.
+ */
+
+class Node
+{
+
+    public int $value;
+    public $next;
+
+    public function __construct(int $value)
+    {
+        $this->value = $value;
+        $this->next = null;
+    }
+
+    public static function insertNodeAtIndexIteratively(Node $head, int $value, int $index): Node
+    {
+        $counter = 0;
+        $currentNode = $head;
+
+        if ($index === 0) {
+            # insert at the beginning of the LL
+            $newNode = new Node($value);
+            $newNode->next = $head;
+            return $newNode;
+        }
+        while ($currentNode !== null && $counter < $index - 1) {
+            # Traverse through the LL, get to the node before insertion point
+            # OR index is out of bound.
+            $currentNode = $currentNode->next;
+            $counter++;
+        }
+
+        if ($currentNode === null) {
+            echo "Index out of bound, cannot insert a new node";
+            exit();
+        }
+
+        # We find the insertion point
+        $newNode = new Node($value);
+        $newNode->next = $currentNode->next;
+        $currentNode->next = $newNode;
+
+        return $head;
+    }
+
+    public static function printLL(Node $head): void
+    {
+        $currentNode = $head;
+        while ($currentNode !== null) {
+            echo "$currentNode->value";
+            $currentNode = $currentNode->next;
+
+            if ($currentNode !== null) echo " --> ";
+        }
+    }
+}
+
+$a = new Node(2);
+$b = new Node(4);
+$c = new Node(6);
+$d = new Node(8);
+
+# a -> b -> c -> d
+$a->next = $b;
+$b->next = $c;
+$c->next = $d;
+
+# Test Run
+$headNode = Node::insertNodeAtIndexIteratively($a, 20, 0);
+Node::printLL($headNode); # output: 20 -> 2 -> 4 -> 6 -> 8
+
+echo "<br>";
+echo "<br>";
+$headNode = Node::insertNodeAtIndexIteratively($headNode, 5, 2);
+Node::printLL($headNode); # output: 20 -> 2 -> 5 -> 4 -> 6 -> 8
