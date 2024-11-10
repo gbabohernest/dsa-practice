8ab99feb0f2f01988f4b885c9e76e1fdee8b9dca
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2024-11-10 23:59:31 +0300

    Refactor, remove class definition

--- PHP/LinkedList/get_node_value.php
+++ PHP/LinkedList/get_node_value.php
@@ -5,55 +5,20 @@
  * there is no node at the given index, then return null.
  */
 
+use LinkedList\NodeDefinition\Node;
 
+function getNodeValue(Node $head, int $index): ?int
+{
 
-class Node {
-    public int $value;
-    public $next;
+    $currentNode = $head;
+    $currentIndex = 0;
 
-    public function __construct(int $value) {
-        $this->value = $value;
-        $this->next = null;
-    }
-
-    public static function getNodeValue(Node $head, int $index) : ?int
+    while ($currentNode !== null)
     {
-        $current_node = $head;
-        $current_index = 0;
-
-        while ($current_node !== null) {
-            if ($current_index === $index) return $current_node->value;
-            $current_node = $current_node->next;
-            $current_index +=1;
-        }
-
-        return null;
-
+        if ($currentIndex === $index) return $currentNode->data;
+        $currentNode = $currentNode->next;
+        $currentIndex += 1;
     }
 
-    public static function getNodeValueRecursively(?Node $head, int $index): ?int {
-
-        if ($head === null) return null;
-        if ($index === 0) return $head->value;
-
-        return self::getNodeValueRecursively($head->next, $index - 1);
-    }
+    return null;
 }
-
-
-$a = new Node(1);
-$b = new Node(2);
-$c = new Node(3);
-$d = new Node (4);
-
-$a->next = $b;   $b->next = $c;  $c->next = $d;
-# a -> b -> c -> d -> null
-
-$result = Node::getNodeValue($a, 1); # Output : 2
-echo "$result";
-
-echo "<br/>";
-$result2 = Node::getNodeValueRecursively($a, 0); # Output: 1
-echo "$result2";
-
-
