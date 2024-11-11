58fcddfc23df4f2f0cf7f84a65da5fbd92ce9f01
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2024-11-11 13:31:42 +0300

    Add LL values to an array and return the array, Recursive and iterative approach

--- PHP/LinkedList/index.php
+++ PHP/LinkedList/index.php
@@ -5,6 +5,7 @@ require_once "../LinkedList/get_node_value.php";
 require_once "../LinkedList/remove_dup_from_sorted_list.php";
 require_once "../LinkedList/insert_at_a_given_index.php";
 require_once  "../LinkedList/linked_find.php";
+require_once  "../LinkedList/linked_list_values.php";
 
 
 use LinkedList\NodeDefinition\Node;
@@ -36,5 +37,11 @@ $nodeValueRec = getNodeValueRecursively($a, 7);
 
 Node::printLL($a);
 echo "<br/>";
-var_dump(LLFindValue($a, 1));# true
-var_dump(LLFindValueRecursively($a, 5)); # false
+
+# var_dump(LLFindValue($a, 1));# true
+# var_dump(LLFindValueRecursively($a, 5)); # false
+
+
+var_dump(LLValuesToArray($a)); # [1, 1, 3, 3]
+echo "<br/>";
+var_dump(LLValuesToArrayRecursively($a)); # [1, 1, 3, 3]
--- PHP/LinkedList/linked_list_values.php
+++ PHP/LinkedList/linked_list_values.php
@@ -5,53 +5,28 @@
  * and return the array containing all the values of a linked list
  */
 
+use LinkedList\NodeDefinition\Node;
 
+/**
+ * @param Node $head : Head of the LL
+ * @return array: An array of LL values
+ */
 
-class Node {
-    public string $value;
-    public $next;
+function LLValuesToArray(Node $head): array
+{
+    $LLValues = [];
+    $currentNode = $head;
 
-    public function __construct(string $value)
-    {
-        $this->value = $value;
-        $this->next = null;
+    while ($currentNode !== null) {
+        $LLValues[] = $currentNode->data;
+        $currentNode = $currentNode->next;
     }
-
-    public static function LL_values_to_array (Node $head) : array
-    {
-        $LL_values = [];
-        $current_node = $head;
-
-        while ($current_node !== null)
-        {
-            $LL_values[] = $current_node->value;
-            $current_node = $current_node->next;
-        }
-        return $LL_values;
-    }
-
-    public static function LL_values_to_array_recursively(?Node $head) : array
-    {
-        if ($head === null) return [];
-        return [$head->value, ...self::LL_values_to_array_recursively($head->next)];
-    }
-
-
+    return $LLValues;
 }
 
-$a = new Node('A');
-$b = new Node('B');
-$c = new Node('C');
-$d = new Node('D');
 
-$a->next = $b;
-$b->next = $c;
-$c->next = $d;
-
-echo "<pre>";
-var_dump(Node::LL_values_to_array($a)); # output: ['A', 'B', 'C', 'D'];
-echo "</pre>";
-
-echo "<pre>";
-var_dump(Node::LL_values_to_array_recursively($a)); # output: ['A', 'B', 'C', 'D'];
-echo "</pre>";
+function LLValuesToArrayRecursively(?Node $head): array
+{
+    if ($head === null) return [];
+    return [$head->data, ...LLValuesToArrayRecursively($head->next)];
+}
