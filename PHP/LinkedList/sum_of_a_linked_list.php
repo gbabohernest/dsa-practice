0674fe48573b23ecb3949f04c9f14f1591307667
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2024-11-12 23:14:55 +0300

    Return the sum of LL values

--- PHP/LinkedList/sum_of_a_linked_list.php
+++ PHP/LinkedList/sum_of_a_linked_list.php
@@ -6,46 +6,22 @@
  */
 
 
-class Node
+use LinkedList\NodeDefinition\Node;
+
+/**
+ * Return the sum of the LL values
+ * @param Node $head: Head Node
+ * @return int: Sum of LL values
+ */
+function getLLSum(Node $head): int
 {
-    public int $value;
-    public $next;
+    $sum = 0;
+    $currentNode = $head;
 
-    public function __construct(int $value)
-    {
-        $this->value = $value;
-        $this->next = null;
+    while ($currentNode !== null) {
+        $sum += $currentNode->data;
+        $currentNode = $currentNode->next;
     }
 
-    public static function get_LL_sum(Node $head): int
-    {
-        $sum = 0;
-        $current_node = $head;
-
-        while ($current_node !== null) {
-            $sum += $current_node->value;
-            $current_node = $current_node->next;
-        }
-
-        return $sum;
-    }
-
-    public static function get_LL_sum_recursively(?Node $head): int
-    {
-        if ($head === null) return 0;
-        return $head->value += self::get_LL_sum_recursively($head->next);
-    }
+    return $sum;
 }
-
-
-$a = new Node(10);
-$b = new Node(30);
-$c = new Node(40);
-$d = new Node(50);
-
-$a->next = $b;
-$b->next = $c;
-$c->next = $d;
-
-var_dump(Node::get_LL_sum($a)); # Output: 130
-var_dump(Node::get_LL_sum_recursively($a)); # Output: 130
