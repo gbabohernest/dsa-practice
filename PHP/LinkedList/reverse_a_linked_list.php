54b9650880775f6339c8733b3d0b48f6f3df063c
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2024-11-12 22:59:20 +0300

    function that reverse a LL and return the new head of the reversed LL

--- PHP/LinkedList/reverse_a_linked_list.php
+++ PHP/LinkedList/reverse_a_linked_list.php
@@ -6,60 +6,20 @@
  * return the new head of the reversed linked list.
  */
 
+use LinkedList\NodeDefinition\Node;
 
-class Node
+
+function reverseLL(Node $head): Node
 {
-    public string $value;
-    public $next;
+    $currentNode = $head;
+    $prevNode = null;
 
-    public function __construct(string $value)
-    {
-        $this->value = $value;
-        $this->next = null;
+    while ($currentNode !== null) {
+        $next = $currentNode->next;
+        $currentNode->next = $prevNode;
+        $prevNode = $currentNode;
+        $currentNode = $next;
     }
 
-    public static function reverse_LL(Node $head): Node
-    {
-        $current_node = $head;
-        $prev_node = null;
-
-        while ($current_node !== null) {
-            $next = $current_node->next;
-            $current_node->next = $prev_node;
-            $prev_node = $current_node;
-            $current_node = $next;
-        }
-
-        return $prev_node;
-    }
-
-    public static function reverse_LL_recursively(?Node $head, $prev_node = null): Node
-    {
-        if ($head === null) return $prev_node;
-        $next = $head->next;
-        $head->next = $prev_node;
-        return self::reverse_LL_recursively($next, $head);
-    }
+    return $prevNode;
 }
-
-$a = new Node('A');
-$b = new Node('B');
-$c = new Node('C');
-$d = new Node('D');
-
-$a->next = $b;
-$b->next = $c;
-$c->next = $d;
-
-
-$result = Node::reverse_LL($a);
-
-echo "<pre>";
-var_dump($result); # D -> C --> B --> A --> NULL
-echo "</pre>";
-
-$result_recursively = Node::reverse_LL_recursively($a);
-
-echo "<pre>";
-var_dump($result_recursively); # D -> C --> B --> A --> NULL
-echo "</pre>";
