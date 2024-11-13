d4c7aef8de88f465451a2a78ed879f1ce4f00047
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2024-11-13 22:36:17 +0300

    Return the sum of LinkedList values

--- JavaScript/LinkedList/index.js
+++ JavaScript/LinkedList/index.js
@@ -1,6 +1,10 @@
 import Node from './Node/Node.js';
 import { getNodeValue, getNodeValueRecursive } from './get_node_value.js';
 import {
+  getSumOfLinkedList,
+  getSumOfLinkedListRecursive,
+} from './sum_of_a_linked_list.js';
+import {
   insertNodeAtIndexRec,
   insertNodeAtIndex,
 } from './insert_a_node_at_index.js';
@@ -24,12 +28,19 @@ d.next = e;
 // Node.printList(removeDupFromSortedLLRecursively(a)); // 1 -> 2 -> 3
 // Node.printList(removeDuplicatesFromSortedLLIteratively(a)); // 1 -> 2 -> 3
 
-console.log(getNodeValue(a, 2)); // 2
-console.log(getNodeValueRecursive(a, 3)); // 3
+//console.log(getNodeValue(a, 2)); // 2
+//console.log(getNodeValueRecursive(a, 3)); // 3
 
-console.log('\n');
+// console.log('\n');
 // insert a new node at index 1
-const newHead = insertNodeAtIndexRec(a, 0, 0);
-Node.printList(newHead); // 0 -> 1 -> 1 -> 2 -> 3 -> 3
+let newHead = insertNodeAtIndexRec(a, 0, 0);
+//Node.printList(newHead); // 0 -> 1 -> 1 -> 2 -> 3 -> 3
 console.log('------------------------------------');
-Node.printList(insertNodeAtIndex(newHead, 6, 4)); // 0 -> 1 -> 1 -> 2 -> 3 -> 3 ->4
+newHead = insertNodeAtIndex(newHead, 6, 4);
+//Node.printList(newHead); // 0 -> 1 -> 1 -> 2 -> 3 -> 3 ->4
+
+/***************************************************** */
+
+console.log('Sum of linked list:');
+console.log(getSumOfLinkedList(newHead)); // 14
+console.log(getSumOfLinkedListRecursive(newHead)); // 14
--- JavaScript/LinkedList/sum_of_a_linked_list.js
+++ JavaScript/LinkedList/sum_of_a_linked_list.js
@@ -3,37 +3,17 @@
  * Iterative & Recursive Approach.
  */
 
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-// create nodes
-const a = new Node(2);
-const b = new Node(4);
-const c = new Node(6);
-const d = new Node(8);
-
-// build up the linked list
-a.next = b;
-b.next = c;
-c.next = d;
-
-//  a  -> b -> c -> d -> null
-
 /**
  * Iterative Approach
- * @param: {Node} head
+ * @param: {NodeDefinition} head
  * @return {number} sum
  */
-const getSumOfLinkedList = (head) => {
+export const getSumOfLinkedList = (head) => {
   let sum = 0;
   let currentNode = head;
 
   while (currentNode !== null) {
-    sum += currentNode.value;
+    sum += currentNode.data;
     currentNode = currentNode.next;
   }
 
@@ -51,10 +31,7 @@ const getSumOfLinkedList = (head) => {
   Time complexity: O(n) -> n is the number of nodes
   space complexity: O(n) -> due to the recursion stack 
 */
-const getSumOfLinkedListRecursive = (head) => {
+export const getSumOfLinkedListRecursive = (head) => {
   if (head === null) return 0;
-  return head.value + getSumOfLinkedListRecursive(head.next);
+  return head.data + getSumOfLinkedListRecursive(head.next);
 };
-
-console.log(getSumOfLinkedList(a));
-console.log(getSumOfLinkedListRecursive(a));
