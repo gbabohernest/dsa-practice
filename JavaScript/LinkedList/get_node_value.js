df0355c945321bccb111e25467de701cf80b41f8
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2024-11-13 15:29:04 +0300

    Return a node's data at a given index

--- JavaScript/LinkedList/get_node_value.js
+++ JavaScript/LinkedList/get_node_value.js
@@ -4,47 +4,15 @@
  * there is no node at the given index, then return null.
  */
 
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-const a = new Node(2);
-const b = new Node(4);
-const c = new Node(22);
-const d = new Node(55);
-
-a.next = b;
-b.next = c;
-c.next = d;
-
-/**
- * @param {Node} head
- * @param {Number} index
- * @return {Number|null}
- */
-const getNodeValue = (head, index) => {
+export const getNodeValue = (head, index) => {
   let currentNode = head;
   let currentIndex = 0;
 
   while (currentNode !== null) {
-    if (currentIndex === index) return currentNode.value;
+    if (currentIndex === index) return currentNode.data;
     currentNode = currentNode.next;
     currentIndex++;
   }
 
   return null;
 };
-
-// Recursive Approach
-
-const getNodeValueRecursive = (head, index) => {
-  if (head === null) return null;
-  if (index === 0) return head.value;
-
-  return getNodeValueRecursive(head.next, index - 1);
-};
-console.log(getNodeValue(a, 2)); // Output: 22
-console.log(getNodeValueRecursive(a, 1)); // output 4
