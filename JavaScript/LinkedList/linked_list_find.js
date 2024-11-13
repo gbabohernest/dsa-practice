c594107b8a31b74b1a0aeaea18555c00a2337fad
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2024-11-13 23:19:41 +0300

    find a value in the LL, return boolean

--- JavaScript/LinkedList/linked_list_find.js
+++ JavaScript/LinkedList/linked_list_find.js
@@ -2,35 +2,17 @@
  * A function that takes the head of Linked List and a target value. The function should return a boolean indicating whether or not the linked list contains the target.
  */
 
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-const a = new Node(2);
-const b = new Node(10);
-const c = new Node(15);
-const d = new Node(30);
-
-a.next = b;
-b.next = c;
-c.next = d;
-
-// a -> b -> c -> d -> null
-
 /**
  *
  * @param {Node} head
  * @param {Number} target
  * @return {Boolean}
  */
-const linkedListFind = (head, target) => {
+export const LLFind = (head, target) => {
   let currentNode = head;
 
   while (currentNode !== null) {
-    if (currentNode.value === target) return true;
+    if (currentNode.data === target) return true;
     currentNode = currentNode.next;
   }
   // we didn't find the target
@@ -40,12 +22,9 @@ const linkedListFind = (head, target) => {
 /********************************************* */
 
 // Recursive Approach
-const recursiveApproach = (head, target) => {
+export const LLFindRec = (head, target) => {
   if (head === null) return false;
-  if (head.value === target) return true;
+  if (head.data === target) return true;
 
-  return recursiveApproach(head.next, target);
+  return LLFindRec(head.next, target);
 };
-
-console.log(linkedListFind(a, 15)); // true
-console.log(recursiveApproach(a, 35)); // false
