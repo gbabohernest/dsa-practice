cb273d866b79d3fdac0c7b4b954af305bef789ea
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2024-11-14 14:23:48 +0300

    Return boolean if a value is find within a linked list or not

--- python/LinkedList/index.py
+++ python/LinkedList/index.py
@@ -3,6 +3,7 @@
 from Node.Node import Node
 from get_node_value import get_node_value, get_node_value_recursively
 from insert_at_a_given_index import insert_at_given_index, insert_at_given_index_recursively
+from linked_list_find import linked_list_find_target, linked_list_find_target_recursively
 
 a = Node(1)
 b = Node(1)
@@ -24,3 +25,5 @@ insert_at_given_index(a, 2, 2)
 Node.print_linked_list(a)  # 1 ==> 1 ==> 2 ==> 2 ==> 3 ==> 3
 insert_at_given_index_recursively(a, 4, 6)
 Node.print_linked_list(a)  # 1 ==> 1 ==> 2 ==> 2 ==> 3 ==> 3 ==> 4
+
+print(linked_list_find_target(a, 4)) # True
--- python/LinkedList/linked_list_find.py
+++ python/LinkedList/linked_list_find.py
@@ -5,25 +5,14 @@ Write a function that takes the head of Linked List and a target value.
 The function should return a boolean indicating weather or not the linked list contains the target.
 """
 
-class Node:
-    def __init__(self, value: int):
-        self.value = value
-        self.next = None
-
-
-a = Node(5)
-b = Node(10)
-c = Node(15)
-d = Node(20)
-
-a.next = b; b.next = c; c.next = d
+from Node.Node import Node
 
 
 def linked_list_find_target(head: Node, target: int) -> bool:
     current_node = head
 
     while current_node is not None:
-        if current_node.value == target: return True
+        if current_node.data == target: return True
         current_node = current_node.next
 
     return False
@@ -33,10 +22,6 @@ def linked_list_find_target(head: Node, target: int) -> bool:
 
 def linked_list_find_target_recursively(head: Node, target: int) -> bool:
     if head is None: return False
-    if head.value == target: return True
+    if head.data == target: return True
 
     return linked_list_find_target_recursively(head.next, target)
-
-# Test run
-print(linked_list_find_target(a, 15)) # True
-print(linked_list_find_target_recursively(a, 25)) # False
