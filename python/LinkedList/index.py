c1cef292eba203264be86b013c50076c89073780
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2024-11-14 14:29:17 +0300

    Return a list of all the values in a linked list

--- python/LinkedList/index.py
+++ python/LinkedList/index.py
@@ -4,6 +4,8 @@ from Node.Node import Node
 from get_node_value import get_node_value, get_node_value_recursively
 from insert_at_a_given_index import insert_at_given_index, insert_at_given_index_recursively
 from linked_list_find import linked_list_find_target, linked_list_find_target_recursively
+from linked_list_values import linked_list_values, linked_list_values_recursively
+
 
 a = Node(1)
 b = Node(1)
@@ -27,3 +29,5 @@ insert_at_given_index_recursively(a, 4, 6)
 Node.print_linked_list(a)  # 1 ==> 1 ==> 2 ==> 2 ==> 3 ==> 3 ==> 4
 
 print(linked_list_find_target(a, 4)) # True
+print(linked_list_values(a)) # [1, 1, 2, 2, 3, 3, 4]
+
--- python/LinkedList/linked_list_values.py
+++ python/LinkedList/linked_list_values.py
@@ -5,37 +5,21 @@ Write a function that add the values of a linked list to an array
 and return the array containing all the values of a linked list
 """
 
+from Node.Node import Node
 from typing import List, Any
-
-class Node:
-    def __init__(self, value: Any):
-        self.value = value
-        self.next = None
-
-
-a = Node('A')
-b = Node('B')
-c = Node('C')
-d  = Node('D')
-
-a.next = b; b.next = c; c.next = d
 
 
 def linked_list_values(head: Node) -> List[Any]:
-        current_node = head
-        values = []
+    current_node = head
+    values = []
 
-        while current_node is not None:
-            values.append(current_node.value)
-            current_node = current_node.next
+    while current_node is not None:
+        values.append(current_node.data)
+        current_node = current_node.next
 
-        return values
+    return values
 
 
 def linked_list_values_recursively(head: Node) -> List[Any]:
     if head is None: return []
-    return [head.value] + linked_list_values_recursively(head.next)
-
-# Test run
-print(linked_list_values(a)) # ['A', 'B', 'C', 'D']
-print(linked_list_values_recursively(a)) # ['A', 'B', 'C', 'D']
+    return [head.data] + linked_list_values_recursively(head.next)
