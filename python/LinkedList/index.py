3a8c9af3cbdd48a8583cb6d29dde58b5e885ed4f
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2024-11-14 14:12:04 +0300

    Add a new node at a given index recursively and iteratively and return the head node

--- python/LinkedList/index.py
+++ python/LinkedList/index.py
@@ -2,6 +2,7 @@
 
 from Node.Node import Node
 from get_node_value import get_node_value, get_node_value_recursively
+from insert_at_a_given_index import insert_at_given_index, insert_at_given_index_recursively
 
 a = Node(1)
 b = Node(1)
@@ -14,6 +15,12 @@ b.next = c
 c.next = d
 d.next = e
 
-Node.print_linked_list(a)
-print(get_node_value_recursively(a, 2))  # 2
-print(get_node_value(a, 3))  # 3
+Node.print_linked_list(a)  # 1 ==> 1 ==> 2 ==> 3 ==> 3
+
+# print(get_node_value_recursively(a, 2))  # 2
+# print(get_node_value(a, 3))  # 3
+
+insert_at_given_index(a, 2, 2)
+Node.print_linked_list(a)  # 1 ==> 1 ==> 2 ==> 2 ==> 3 ==> 3
+insert_at_given_index_recursively(a, 4, 6)
+Node.print_linked_list(a)  # 1 ==> 1 ==> 2 ==> 2 ==> 3 ==> 3 ==> 4
--- python/LinkedList/insert_at_a_given_index.py
+++ python/LinkedList/insert_at_a_given_index.py
@@ -4,110 +4,71 @@
 Given the head of a LL, an index, and a value. Write a function that insert a given
 node in the  LL at a given index and return the head of the List.
 """
+from Node.Node import Node
 
-from typing import Any
 
+def insert_at_given_index_recursively(head: Node, value: int, index: int) -> Node or None:
+    """
+    Recursively insert a new node at a given index in the single LL
+    :param head: current head of the LL
+    :param value: value to insert
+    :param index: index at which to insert the value
+    :return: updated head of the LL
 
-class Node:
-    def __init__(self, data: int):
-        self.data = data
-        self.next = None
+    """
 
-    @staticmethod
-    def insert_at_given_index_recursively(head: 'Node', value: int, index: int) -> 'Node' or None:
-        """
-        Recursively insert a new node at a given index in the single LL
-        :param head: current head of the LL
-        :param value: value to insert
-        :param index: index at which to insert the value
-        :return: updated head of the LL
-
-        """
-
-        if head is None:
-            print('Index out of bound, cannot insert')
-            exit()
-
-        if head is None and index == 0:
-            # insert at the beginning of the list
-            new_node = Node(value)
-            new_node.next = head
-            head = new_node
-            return head
-
-        if index == 0:
-            new_node = Node(value)
-            new_node.next = head
-            return new_node
-
-        head.next = Node.insert_at_given_index_recursively(head.next, value, index - 1)
+    if head is None and index == 0:
+        # insert at the beginning of the list
+        new_node = Node(value)
+        new_node.next = head
+        head = new_node
         return head
 
-    @staticmethod
-    def insert_at_given_index_iteratively(head: 'Node', value: int, index: int) -> 'Node':
-        """
-        Inserts a node in a given LL at a particular index.
+    if index == 0:
+        new_node = Node(value)
+        new_node.next = head
+        return new_node
 
-        :param head: current head of the LL
-        :param value: value to insert
-        :param index: index at which to insert the value
-        :return: updated head of the LL
-        """
+    head.next = insert_at_given_index_recursively(head.next, value, index - 1)
+    return head
 
-        if index == 0:
-            # Insert at the beginning of the LL
-            new_node = Node(value)
-            new_node.next = head
-            head = new_node
-            return head
 
-        prev_node = None
-        counter = 0
-        current_node = head
+def insert_at_given_index(head: Node, value: int, index: int) -> Node:
+    """
+    Inserts a node in a given LL at a particular index.
 
-        while current_node is not None and counter < index:
-            # Traverse the LL to find the insertion point or Not
-            prev_node = current_node
-            current_node = current_node.next
-            counter += 1
+    :param head: current head of the LL
+    :param value: value to insert
+    :param index: index at which to insert the value
+    :return: updated head of the LL
+    """
 
-        if counter == index:
-            # Insertion point is found
-            new_node = Node(value)
-            prev_node.next = new_node
-            new_node.next = current_node
-
-        else:
-            # index is out of bound
-            print('Index is out of bound, cannot insert a new node')
-            exit()
-
+    if index == 0:
+        # Insert at the beginning of the LL
+        new_node = Node(value)
+        new_node.next = head
+        head = new_node
         return head
 
-    @staticmethod
-    def print_linkedlist(head_node: 'Node') -> None:
+    prev_node = None
+    counter = 0
+    current_node = head
 
-        current_node = head_node
-        while current_node is not None:
-            print(current_node.data)
-            current_node = current_node.next
+    while current_node is not None and counter < index:
+        # Traverse the LL to find the insertion point or Not
+        prev_node = current_node
+        current_node = current_node.next
+        counter += 1
 
+    if counter == index:
+        # Insertion point is found
+        new_node = Node(value)
+        prev_node.next = new_node
+        new_node.next = current_node
 
-# create nodes
-a = Node(1)
-b = Node(2)
-c = Node(3)
-d = Node(4)
-f = Node(6)
-# a -> b -> c -> d -> f
-a.next = b
-b.next = c
-c.next = d
-d.next = f
+    else:
+        # index is out of bound
+        print('Index is out of bound, cannot insert a new node')
+        exit()
 
-# Test
-Node.print_linkedlist(Node.insert_at_given_index_iteratively(a, 5, 4))  # 1 -> 2 -> 3 -> 4 -> 5 -> 6
-print()
-Node.print_linkedlist(Node.insert_at_given_index_iteratively(a, 0, 0))  # 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6
-print()
-Node.print_linkedlist(Node.insert_at_given_index_recursively(a, 55, 3))  # 1 -> 2 -> 3 -> 55 -> 4 -> 5 -> 6
+    return head
